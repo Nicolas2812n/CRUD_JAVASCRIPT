6380db65c575d7c01c2a2b01a97a05fa06d3d33c
diego <diegoalexanderpi1@gmail.com>
2023-09-18 14:24:56 -0500

    probando el pase de parametros

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,15 +2,20 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
-app.get('/:id', (req, res)=>{
-    const { id } = req.params
-    res.send('Peticion Get');
+
+app.get('/:name/:id', (req, res)=>{
+    const { name,id } = req.params;
+
+    res.send(`${name}: ${id}`);
 });
 
 app.post('/', (req, res)=>{
-    res.send('Peticion POST');
+  const { email, password } = req.body;
+  res.send (`${email}: ${password}`);
 });
 
 app.delete('/', (req, res)=>{
