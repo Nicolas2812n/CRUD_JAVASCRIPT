cf3f3826079f4ff39e291e19f1e4af602fb95b32
diego <diegoalexanderpi1@gmail.com>
2023-09-18 14:07:04 -0500

    explicando destructutring en javascript

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,26 @@ const express = require('express');
 
 const app = express();
 
+
+
+app.get('/:id', (req, res)=>{
+    const { id } = req.params
+    res.send('Peticion Get');
+});
+
+app.post('/', (req, res)=>{
+    res.send('Peticion POST');
+});
+
+app.delete('/', (req, res)=>{
+    res.send('Peticion DELETE');
+});
+
+app.put('/', (req, res)=>{
+    res.send('Peticion PUT');
+});
+
+
 app.listen(3000, ()=>{
     console.log('servidor encendido');
 });
