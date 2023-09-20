fd25bb52348b0772066a339b7bf5ba970020ffe0
Nicolas <Nicolas.albagaulteros@gmail.com>
2023-09-20 16:39:02 -0500

    funcion para crear usuario

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -1,15 +1,38 @@
-// CRUD
+const database = require('../config/database');
+const mysql2 = require('mysql2');
+
 
 const readUser = (req, res ) => {
     const { id } = req.params;
 
+    const readQuery = `SELECT * FROM User WHERE id=?;`;
+
+    const query = mysql2.format(readQuery, [id]);
     
+    database.query(query, (err, result)=>{
+        if(err) throw err;
+        if (result[0] !== undefined){
+            res.json(result[0]);   
+        }else{
+          res.json({massage: 'Usuario no encontrado'});     
+        }
+    });
 };
 
 const createUser = (req, res) => {
-    const { email, password } = req.body;
-    res.send (`${email}: ${password}`);
-};
+    const { first_name, age } = req.body;
+
+    const createQuery = `INSERT INTO USER(first_name, age) VALUE(?, ?)`;
+
+const query = mysql2.format(createQuery, [first_name, age]);
+
+database.query(query, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send({ massage: 'Usuario creado' });
+    });
+
+}; 
 
 const updateUser = (req, res) => {
     res.send('Peticion PUT');
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -9,7 +9,7 @@ const {
 
 const router = Router();
 
-router.get('//:id', readUser);
+router.get('/:id', readUser);
 
 router.post('/', createUser);
 
